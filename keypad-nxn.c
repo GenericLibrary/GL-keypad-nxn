9cec6da2aca71e4d8f611c367deec60e172bcb06
roquita <bill.egoavil.m@gmail.com>
2021-11-25 01:19:56 -0500

    .

---
+++ keypad-nxn.c
@@ -0,0 +1,43 @@
+#include "keypad-nxn.h"
+
+void init_keypad(keypad_t *keypad)
+{
+    uint8_t rows = keypad->rows;
+    for (uint8_t i = 1; i <= rows; i++)
+    {
+        keypad->row_as_Input(i);
+    }
+}
+
+keypad_keyPos_t scan_keypad(keypad_t *keypad)
+{
+    keypad_keyPos_t result = {-1, -1};
+    uint8_t rows = keypad->rows;
+    uint8_t columns = keypad->columns;
+
+    for (uint8_t i = 0; i < rows; i++)
+    {
+        keypad->row_as_OutputLow(i);
+
+        bool key_pressed = false;
+        for (uint8_t j = 0; j < columns; j++)
+        {
+            key_pressed = (keypad->get_column_value(j) == 0);
+            if (key_pressed)
+            {
+                result.row = i;
+                result.column = j;
+                break;
+            }
+        }
+
+        keypad->row_as_Input(i);
+
+        if (key_pressed)
+        {
+            break;
+        }
+    }
+
+    return result;
+}
---
+++ keypad-nxn.h
@@ -0,0 +1,29 @@
+#ifndef _KEYPAD_NXN_H_
+#define _KEYPAD_NXN_H_
+
+#include "stdint.h"
+#include "stdbool.h"
+
+typedef struct
+{
+    int row;
+    int column;
+} keypad_keyPos_t;
+
+typedef void (*keypad_row_as_Input_fn)(uint8_t row);           //set by user
+typedef void (*keypad_row_as_OutputLow_fn)(uint8_t row);       //set by user
+typedef uint8_t (*keypad_get_column_value_fn)(uint8_t column); //set by user
+
+typedef struct
+{
+    uint8_t rows;
+    uint8_t columns;
+    keypad_row_as_Input_fn row_as_Input;
+    keypad_row_as_OutputLow_fn row_as_OutputLow;
+    keypad_get_column_value_fn get_column_value;
+} keypad_t;
+
+void init_keypad(keypad_t *keypad);
+keypad_keyPos_t scan_keypad(keypad_t *keypad);
+
+#endif /* _KEYPAD_NXN_H_ */
