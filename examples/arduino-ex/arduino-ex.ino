daa6833f032e786b54fe6d0ea44205ca89c1386f
GenericLibrary <sanqtsax@gmail.com>
2021-12-19 17:01:09 -0500

    arduino ex added

--- examples/arduino-ex/arduino-ex.ino
+++ examples/arduino-ex/arduino-ex.ino
@@ -1,22 +1,114 @@
 #include "keypad-nxn.h"
 
 #define ROWS_NUMBER 4
-#define ROW1 18
-#define ROW2 19
-#define ROW3 21
-#define ROW4 22
+#define ROW1 22
+#define ROW2 24
+#define ROW3 26
+#define ROW4 28
 #define COLS_NUMBER 4
-#define COL1 15
-#define COL2 23
-#define COL3 4
-#define COL4 5
+#define COL1 30
+#define COL2 32
+#define COL3 34
+#define COL4 36
 
-void setup() {
-  // put your setup code here, to run once:
+keypad_t mykeypad;
 
+void row_as_input(uint8_t row)
+{
+  switch (row)
+  {
+  case 1:
+    pinMode(ROW1, INPUT);
+    break;
+  case 2:
+    pinMode(ROW2, INPUT);
+    break;
+  case 3:
+    pinMode(ROW3, INPUT);
+    break;
+  case 4:
+    pinMode(ROW4, INPUT);
+    break;
+  default:
+    break;
+  }
+}
+void row_as_output_low(uint8_t row)
+{
+  switch (row)
+  {
+  case 1:
+    pinMode(ROW1, OUTPUT);
+    digitalWrite(ROW1, LOW);
+    break;
+  case 2:
+    pinMode(ROW2, OUTPUT);
+    digitalWrite(ROW2, LOW);
+    break;
+  case 3:
+    pinMode(ROW3, OUTPUT);
+    digitalWrite(ROW3, LOW);
+    break;
+  case 4:
+    pinMode(ROW4, OUTPUT);
+    digitalWrite(ROW4, LOW);
+    break;
+  default:
+    break;
+  }
+}
+uint8_t get_column_value(uint8_t column)
+{
+  switch (column)
+  {
+  case 1:
+    return (uint8_t)digitalRead(COL1);
+  case 2:
+    return (uint8_t)digitalRead(COL2);
+  case 3:
+    return (uint8_t)digitalRead(COL3);
+  case 4:
+    return (uint8_t)digitalRead(COL4);
+  default:
+    return 0;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+  // Init peripherals asociated to the keypad:
+  // all columns as "INPUT PULL_UP"
+  pinMode(COL1, INPUT_PULLUP);
+  pinMode(COL2, INPUT_PULLUP);
+  pinMode(COL3, INPUT_PULLUP);
+  pinMode(COL4, INPUT_PULLUP);
 
+  // all rows as " FLOATING INPUT"
+  pinMode(ROW1, INPUT);
+  pinMode(ROW2, INPUT);
+  pinMode(ROW3, INPUT);
+  pinMode(ROW4, INPUT);
+
+  // Create a variable of type "keypad_t" and define each of its attributes  
+  mykeypad.rows = ROWS_NUMBER;
+  mykeypad.columns = COLS_NUMBER;
+  mykeypad.row_as_Input = row_as_input;
+  mykeypad.row_as_OutputLow = row_as_output_low;
+  mykeypad.get_column_value = get_column_value;
+
+  // Call "init_keypad" to initialize the keypad. In this case it does nothing
+  init_keypad(&mykeypad);
+
+  printf("\nPRESS KEYS:\n");
+}
+
+void loop()
+{
+  //Call "scan_keypad" to check if any key is pressed
+  keypad_keyPos_t key = scan_keypad(&mykeypad);
+  if (key.row != -1 && key.column != -1)
+  {
+    printf("key pressed: %d, %d\n", key.row, key.column);
+  }
+  delay(1000);
 }
--- keypad-nxn.h
+++ keypad-nxn.h
@@ -4,26 +4,35 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-typedef struct
+#ifdef __cplusplus
+extern "C"
 {
-    int row;
-    int column;
-} keypad_keyPos_t;
+#endif
 
-typedef void (*keypad_row_as_Input_fn)(uint8_t row);           //set by user
-typedef void (*keypad_row_as_OutputLow_fn)(uint8_t row);       //set by user
-typedef uint8_t (*keypad_get_column_value_fn)(uint8_t column); //set by user
+    typedef struct
+    {
+        int row;
+        int column;
+    } keypad_keyPos_t;
 
-typedef struct
-{
-    uint8_t rows;
-    uint8_t columns;
-    keypad_row_as_Input_fn row_as_Input;
-    keypad_row_as_OutputLow_fn row_as_OutputLow;
-    keypad_get_column_value_fn get_column_value;
-} keypad_t;
+    typedef void (*keypad_row_as_Input_fn)(uint8_t row);           //set by user
+    typedef void (*keypad_row_as_OutputLow_fn)(uint8_t row);       //set by user
+    typedef uint8_t (*keypad_get_column_value_fn)(uint8_t column); //set by user
 
-void init_keypad(keypad_t *keypad);
-keypad_keyPos_t scan_keypad(keypad_t *keypad);
+    typedef struct
+    {
+        uint8_t rows;
+        uint8_t columns;
+        keypad_row_as_Input_fn row_as_Input;
+        keypad_row_as_OutputLow_fn row_as_OutputLow;
+        keypad_get_column_value_fn get_column_value;
+    } keypad_t;
+
+    void init_keypad(keypad_t *keypad);
+    keypad_keyPos_t scan_keypad(keypad_t *keypad);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _KEYPAD_NXN_H_ */
