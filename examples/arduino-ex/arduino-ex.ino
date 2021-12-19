8c69847829107db4f27fff6cb402682e2f06dd8f
GenericLibrary <sanqtsax@gmail.com>
2021-12-19 16:51:26 -0500

    arduino ex added

---
+++ examples/arduino-ex/arduino-ex.ino
@@ -0,0 +1,22 @@
+#include "keypad-nxn.h"
+
+#define ROWS_NUMBER 4
+#define ROW1 18
+#define ROW2 19
+#define ROW3 21
+#define ROW4 22
+#define COLS_NUMBER 4
+#define COL1 15
+#define COL2 23
+#define COL3 4
+#define COL4 5
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
